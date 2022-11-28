ef974ce61858fb41658cf0290b8d37023fcfc706
VictorGiraldoG <104600192+Torvic1266@users.noreply.github.com>
2022-11-27 21:22:26 -0500

    se sube fondo y parte del header

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express=require("express");
+const { dirname } = require("path");
 const path =require("path");
 const app = express();
+
+const publicFolderPath = path.resolve(__dirname, "./public");
+app.use( express.static(publicFolderPath));
 
 app.listen(3000,() => {
     console.log("servidor funcionando")
